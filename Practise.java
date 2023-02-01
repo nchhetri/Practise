3148b3e32c5754a259a254c63fc6b2b8b8c90e40
NihalChhetri <nc51175@gmail.com>
2023-02-01 22:41:14 +0530

    first commit

---
+++ Practise.java
@@ -0,0 +1,22 @@
+public class Practise
+{
+	public static void main(String args[])
+	{
+		int choice;
+		for (int i=1; i<=2; i++)
+		{
+			choice = i; 
+			switch (choice)
+			{
+				case 1:
+					int a = 10;
+					System.out.println(a);
+					break;
+				case 2:
+					a = 20;
+					System.out.println(a);
+					break;
+			}
+		}
+	}
+}
